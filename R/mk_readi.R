f7c37173158d1a8473163e18f9d3218b480bb075
Meera Patel <patelm9@mskcc.org>
2020-04-20 19:22:30 -0400

    automated some READI parameters

--- R/mk_readi.R
+++ R/mk_readi.R
@@ -1,21 +1,20 @@
 #' Create a Readi
-#' A Readi is an S4 object that contains a dataframe of File Details elements pertaining to a specific csv or Excel file os well as the entire README contents of the working directory. This function makes a new Readi S4 Class object using the given parameters.
+#' @description A Readi is an S4 object that contains a dataframe of File Details elements pertaining to a specific csv or Excel file os well as the entire README contents of the working directory. This function makes a new Readi S4 Class object using the given parameters.
 #' This is first achived by:
 #' 1. Making a README.md in the working directory if one does not exist, followed by:
 #' 2. Adding a File Details section in the README if that does not exist.
 #' @importFrom readr read_lines
 #' @importFrom tidyr separate
 #' @importFrom dplyr tibble
+#' @importFrom cave present_script_path
+#' @importFrom glitter remote_url
 #' @export
 
 mk_readi <-
     function(sources,
              summary,
-             path_to_local_r_script,
-             github_link_to_r_script,
              path_to_original_output_file,
-             notes,
-             log_details) {
+             notes) {
 
         ##Making sure the 2 prerequisites for a Readi object are present:
         ##1. README in the working directory
@@ -39,16 +38,21 @@ mk_readi <-
             bottom_part <- "\n"
         }
 
+        ##Getting GitHub link if there isn't a remote, returns a message say so
+        github_link <- glitter::remote_url()
+        if (length(github_link) == 0) {
+            github_link <- paste0("No remote associated with working directory '", getwd(), "'")
+        }
+
         ##Writing middle part
         middle_part <- c(
             paste0("Timestamp:\t", stampede::stamp_this()),
             paste0("Data Sources:\t", sources),
             paste0("Summary:\t", summary),
-            paste0("Local R Script:\t", path_to_local_r_script),
-            paste0("GitHub Link:\t", github_link_to_r_script),
+            paste0("Local R Script:\t", cave::present_script_path()),
+            paste0("GitHub Link:\t", github_link),
             paste0("Original Output File:\t", path_to_original_output_file),
-            paste0("Notes:\t", notes),
-            paste0("Log Details:\t", log_details))
+            paste0("Notes:\t", notes))
 
         ##Creating a concurrent dataframe
         middle_part_df <-
