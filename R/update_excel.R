98bda6e840397e8208313ae4749c146d03e964dc
Meera Patel <patelm9@mskcc.org>
2020-05-10 18:15:21 -0400

    bug fix update_excel function

--- R/update_excel.R
+++ R/update_excel.R
@@ -6,10 +6,20 @@
 update_excel <-
     function(readi_object, path_to_excel) {
 
-        xlsx::write.xlsx(x = readi_object@dataframe,
-                         file = path_to_excel,
-                         sheetName = "File Details",
-                         row.names = FALSE,
-                         append = TRUE)
+        excel <- broca::read_full_excel(path_to_excel)
+        if ("File Details" %in% names(excel)) {
+            excel[["File Details"]] <-
+                dplyr::bind_rows(excel[["File Details"]],
+                                  readi_object@dataframe)
+
+            broca::write_full_excel(x = excel,
+                                    file = path_to_excel)
+        } else {
+            excel[[length(excel)+1]] <- readi_object@dataframe
+            names(excel)[length(excel)] <- "File Details"
+
+            broca::write_full_excel(x = excel,
+                                    file = path_to_excel)
+        }
 
     }
