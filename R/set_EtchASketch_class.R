a8af529f10273cbb5c78ceff117857bfec636038
Meera Patel <patelm9@mskcc.org>
2020-05-14 00:29:08 -0400

    add Trace, Bookmark, and EtchASketch S4 Class functions

--- R/set_EtchASketch_class.R
+++ R/set_EtchASketch_class.R
@@ -1,6 +1,5 @@
 #' Set EtchASketch S4 Class
-#' @export EtchASketch
-#' @exportClass EtchASketch
+
 
 EtchASketch <- setClass("EtchASketch",
                         contains = c("Trace", "Bookmark"))
